257331d0bcf31abe4ac845e68717eece2b880f81
LunaBrisa <danielalunacast0108@gmail.com>
2024-07-24 16:05:52 -0600

    se modifico menu

--- src/views/Cliente/menuView.vue
+++ src/views/Cliente/menuView.vue
@@ -1,55 +1,114 @@
 <template>
-  <div class="menu-container">
+   <div class="menu-container">
     <BarNav />
     <v-row class="menu" align="center" justify="center"></v-row>
-    <div style="margin-top: 20px;" class="content-container">
-      <v-row justify="center">
+    <div style="margin-top: 20px; " class="content-container">
+      <v-row  justify="center">
+      
         <v-card-title class="title">
           <h1>PLATILLOS PRINCIPALES</h1>
         </v-card-title>
       </v-row>
       <v-row dense justify="center">
         <v-col cols="4" class="bordered">
-          <h1>ORDEN DE TACOS DORADOS <br>(8 PZAS.)</h1>
+          <h1>ORDEN DE TACOS DORADOS (8 PZAS.)</h1>
+          <v-img class="logo" :src="tacos" contain></v-img>
      
-          <h2>Tacos de guiso a elección debajo <br> de una cama de deliciosa ensalada, <br> cueritos y crema</h2>
+          <h2>Tacos de guiso a elección <br> debajo  de una cama de <br> deliciosa ensalada,<br>  cueritos y crema</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$152.00</h1>
         </v-col>
         <v-col cols="4" class="bordered">
           <h1>POLLO A LA PLANCHA</h1>
+          <v-img  style="position: absolute; height: 100px; width: 150px; border-radius: 25px; margin-left: 295px; border: solid 3px black;" :src="pollo" contain></v-img>
          
           <h2>Fajitas de pollo acompañado <br> de verduras en vinagre, <br> ensalada y deliciosa pasta.</h2>
-          <h1 style="width: 12px; padding-left: 70px; margin-top: 20px; font-size: 25px;">$152.00</h1>   
+          <h1 style="width: 12px; padding-left: 70px; margin-top: 40px; font-size: 25px;">$152.00</h1>   
         </v-col>
         <v-col cols="4" class="bordered">
           <h1>TACOS DE PESCADO (4PZ)</h1>
+          <v-img  style="position: absolute; height: 100px; width: 150px; border-radius: 25px; margin-left: 295px; border: solid 3px black;" :src="pescado" contain></v-img>
+         
         
           <h2>Tacos de pescado acompañados<br>de salchicha asada, <br> ensalada y cebolla asada</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$90.00</h1>
         </v-col><v-col cols="4" class="bordered">
           <h1>GRINGA CARNE ASADA</h1>
+          <v-img  style="position: absolute; height: 100px; width: 150px; border-radius: 25px; margin-left: 295px; border: solid 3px black;" :src="gringa" contain></v-img>
+         
        
           <h2>Tortilla de harina gigante con <br> carne asada y queso, <br> acompañada de ensalada</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$150.00</h1>
         </v-col>
         <v-col cols="4" class="bordered">
           <h1>ORDEN DE TOSTADAS(3Pz)</h1>
+          <v-img  style="position: absolute; height: 100px; width: 150px; border-radius: 25px; margin-left: 295px; border: solid 3px black;" :src="tostadas" contain></v-img>
+         
    
           <h2>Deliciosas tostadas en una cama<br>de frijoles con el guiso<br>de su eleccion, acompañado de<br> ensalada, cueritos, queso y crema.</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$90.00</h1>
         </v-col><v-col cols="4" class="bordered">
           <h1>HAMBURGUESA DE LA CASA</h1>
-        
+          <v-img  style="position: absolute; height: 100px; width: 150px; border-radius: 25px; margin-left: 295px; border: solid 3px black;" :src="hamburgesa" contain></v-img>
+         
           <h2>Jugosa carne premiun, jamón, <br> ensalada y aderezos.</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$100.00</h1>
         </v-col>
+       <v-row  justify="center">
+        <v-col cols="2" class="guiso">
+          <h1>NUESTROS GUISOS 
+            <br>A ELECCION</h1>
+      
+            <li>Rajas</li>
+            <li>Pollo</li>
+            <li>Picadillo</li>
+            <li>Chicharron</li>
+            <li>Frijoles</li>
+            <li>Requeson</li>
+            <li>Desebrada</li>
+            <li>Papa c/queso</li>
+        </v-col>
+        <v-col cols="2" class="guiso">
+          <h1>BEBIDAS</h1>
+            <ul><b>Aguas</b></ul>
+            <li>Agua del Día </li>
+            <li>Agua Mineral </li>
+            <li>Agua de Jamaica </li>
+            <li>Agua de Horchata</li>
+            <ul><b>Refrescos</b></ul>
+            <li>Coca Cola</li>
+            <li>Fanta </li>
+            <li>Manzanita</li>
+        </v-col>
+        <v-col cols="2" class="guiso">
+          <h1>POSTRES</h1>
+            <ul><b>Pays</b></ul>
+            <li>Limón </li>
+            <li>Fresa </li>
+            <li>Queso </li>
+            <ul> <b>Pastel</b></ul>
+            <li>Chocolate</li>
+            <li>Zanahoria </li>
+            <li>Chorreado</li>
+        </v-col>
+       </v-row>
       </v-row>
+
+         <v-row justify="center">
+          <RouterLink to="/reserva"><v-col  cols="2" class="boton"><button @click="ordenar" style="color: black; margin-left:-5px;" >ORDENAR</button> </v-col></RouterLink>
+          </v-row>     
     </div>
-  </div>
+   </div>
+
 </template>
 
 <script setup>
 import BarNav from '@/components/barNav.vue';
+import tacos from '@/assets/tacos.jpg';
+import pollo from '@/assets/pollo.jpeg';
+import  pescado from '@/assets/tacos de pescado.jpeg';
+import  gringa from '@/assets/gringa carne asada.png';
+import  tostadas from '@/assets/tostadas.jpg';
+import  hamburgesa from '@/assets/hamburgesa.jpeg';
 </script>
 
 <style scoped>
@@ -67,9 +126,16 @@ import BarNav from '@/components/barNav.vue';
 .bordered {
   border: 2px solid #ff4f09;
   border-radius: 15px;
-  margin: 70px;
-  width: 500px;
+  margin: 40px;
+  width: 400px;
   height: 200px;
+  background-color: white;
+}
+.guiso{
+  border: 2px solid #ff4f09;
+  border-radius: 15px;
+  margin: 80px;
+  height: 300px;
 }
 h1 {
   font-family: monospace;
@@ -82,4 +148,25 @@ h2 {
   color: #808080;
   margin: 0%;
 }
+.boton{
+  border: 2px solid #ff4f09;
+  border-radius: 15px;
+  height: 100px;
+  width:700px;
+  background-color: #ff4f09;
+  font-family: monospace;
+  font-size: 50px;
+  align-items: center;
+  margin-left: 550px;
+}
+.logo{
+  width: 150px;
+  border-radius: 25px;  
+  margin-left: 300px;
+  margin-top: 5px;
+  position: absolute;
+  border: 2px solid black;
+}
+
+
 </style>

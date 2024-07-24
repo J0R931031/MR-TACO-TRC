ac79f7ebf4038d55bf3b26075145bd41a37dc94c
LunaBrisa <danielalunacast0108@gmail.com>
2024-07-23 23:04:56 -0600

    modificacion

--- src/views/Cliente/menuView.vue
+++ src/views/Cliente/menuView.vue
@@ -8,29 +8,40 @@
           <h1>PLATILLOS PRINCIPALES</h1>
         </v-card-title>
       </v-row>
-      <v-row justify="center">
+      <v-row dense justify="center">
         <v-col cols="4" class="bordered">
           <h1>ORDEN DE TACOS DORADOS <br>(8 PZAS.)</h1>
-          <br>
+     
           <h2>Tacos de guiso a elección debajo <br> de una cama de deliciosa ensalada, <br> cueritos y crema</h2>
           <h1 style="padding-left: 80px; font-size: 25px;">$152.00</h1>
         </v-col>
         <v-col cols="4" class="bordered">
           <h1>POLLO A LA PLANCHA</h1>
-          <br>
+         
           <h2>Fajitas de pollo acompañado <br> de verduras en vinagre, <br> ensalada y deliciosa pasta.</h2>
-          <h1 style="width: 12px; padding-left: 70px; margin-top: 20px; font-size: 25px;">$152.00</h1>
+          <h1 style="width: 12px; padding-left: 70px; margin-top: 20px; font-size: 25px;">$152.00</h1>   
         </v-col>
         <v-col cols="4" class="bordered">
-          <h1>ORDEN DE TACOS DORADOS <br>(8 PZAS.)</h1>
-          <br>
-          <h2>Tacos de guiso a elección debajo <br> de una cama de deliciosa ensalada, <br> cueritos y crema</h2>
-          <h1 style="padding-left: 80px; font-size: 25px;">$152.00</h1>
+          <h1>TACOS DE PESCADO (4PZ)</h1>
+        
+          <h2>Tacos de pescado acompañados<br>de salchicha asada, <br> ensalada y cebolla asada</h2>
+          <h1 style="padding-left: 80px; font-size: 25px;">$90.00</h1>
         </v-col><v-col cols="4" class="bordered">
-          <h1>ORDEN DE TACOS DORADOS <br>(8 PZAS.)</h1>
-          <br>
-          <h2>Tacos de guiso a elección debajo <br> de una cama de deliciosa ensalada, <br> cueritos y crema</h2>
-          <h1 style="padding-left: 80px; font-size: 25px;">$152.00</h1>
+          <h1>GRINGA CARNE ASADA</h1>
+       
+          <h2>Tortilla de harina gigante con <br> carne asada y queso, <br> acompañada de ensalada</h2>
+          <h1 style="padding-left: 80px; font-size: 25px;">$150.00</h1>
+        </v-col>
+        <v-col cols="4" class="bordered">
+          <h1>ORDEN DE TOSTADAS(3Pz)</h1>
+   
+          <h2>Deliciosas tostadas en una cama<br>de frijoles con el guiso<br>de su eleccion, acompañado de<br> ensalada, cueritos, queso y crema.</h2>
+          <h1 style="padding-left: 80px; font-size: 25px;">$90.00</h1>
+        </v-col><v-col cols="4" class="bordered">
+          <h1>HAMBURGUESA DE LA CASA</h1>
+        
+          <h2>Jugosa carne premiun, jamón, <br> ensalada y aderezos.</h2>
+          <h1 style="padding-left: 80px; font-size: 25px;">$100.00</h1>
         </v-col>
       </v-row>
     </div>
@@ -63,10 +74,12 @@ import BarNav from '@/components/barNav.vue';
 h1 {
   font-family: monospace;
   font-size: 20px;
+  margin: 10px;
 }
 h2 {
   font-family: monospace;
   font-size: 15px;
   color: #808080;
+  margin: 0%;
 }
 </style>
